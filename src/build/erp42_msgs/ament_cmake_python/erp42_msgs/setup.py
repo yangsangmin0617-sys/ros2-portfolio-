75ac72613fd84680226029c5481e65a34b696d56
yangsangmin0617-sys <yangsangmin0617@gmail.com>
2026-03-24 20:50:40 +0900

    Delete src directory

--- src/build/erp42_msgs/ament_cmake_python/erp42_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='erp42_msgs',
-    version='0.0.0',
-    packages=find_packages(
-        include=('erp42_msgs', 'erp42_msgs.*')),
-)
--- src/build/my_robot/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/yangsangmin/ws/src/install/erp42_msgs/share/erp42_msgs/package.sh"
